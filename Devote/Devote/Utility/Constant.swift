cf35199931969cea43c9e0357bc8545b7868c4c3
Jon.Olivet <jon.olivet@publix.com>
2025-02-28 16:35:22 -0500

    Devote progress

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Jon Olivet on 2/28/25.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
